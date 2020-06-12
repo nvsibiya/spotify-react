7bcb8330366d3caffd136a5c154312f043a1e2d7
Nomfundo Verah Sibiya <nvsibiya0@gmail.com>
2020-06-12 23:20:48 +0200

    Black navbar

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Navbar, Nav, Image, Button, FormControl, Form } from 'react-bootstrap'
 // import { withRouter } from "react-router";
-import '../App.css';
+
 
 
 const SideNav = () => {
   return (
-    <Navbar expand='md' className="navbar navbar-dark fixed-top vertical-navbar flex-column" bg="dark">
+    <Navbar expand='md' className="navbar navbar-dark fixed-top vertical-navbar flex-column" >
     <Navbar.Brand href="#home" className='my-3'>
         <Image src="/assets/spotify-logo.png" alt="Spotify logo" />
     </Navbar.Brand>
@@ -19,9 +19,9 @@ const SideNav = () => {
                 <Nav.Link className="nav-item nav-link align-items-center" href="#"><i className="fas fa-book-open"></i>Pricing</Nav.Link>
             </div>
             <div>
-                <Nav.Link className="nav-item nav-link align-items-center" href="https://play.google.com/store/apps/details?id=com.spotify.music"><i className="far fa-arrow-alt-circle-down"></i>Install App</Nav.Link>
+                <Nav.Link className="nav-item nav-link align-items-center" href="#"><i className="far fa-arrow-alt-circle-down"></i>Install App</Nav.Link>
                 <hr className="d-flex flex-wrap" />
-                <Nav.Link className="nav-item nav-link align-items-center" href="#"><i className="fas fa-user"></i>User Name</Nav.Link>
+                <Nav.Link className="nav-item nav-link align-items-center" href="#"><i className="fas fa-user"></i>Username</Nav.Link>
             </div>
         </div>
     </Navbar.Collapse>
